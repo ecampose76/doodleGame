2571b29691e7fee8c05fe6611b5fec439fc18985
Elias Campos <ecampose76@gmail.com>
2020-12-29 21:41:45 -0600

    added 10 elements to each WH card

--- doodleBuzz.js
+++ doodleBuzz.js
@@ -1,6 +1,6 @@
-const who = ['Homer Simpson', 'A deep sea diver', 'A dentist']
-const what = ['knitting a sweater', 'petting a cat', 'picking flowers']
-const where = ['at a bank.', 'at the hairdresser.', 'in a library.']
+const who = ['Homer Simpson', 'A deep sea diver', 'A dentist', 'Mickey Mouse', 'A basketball player', 'A lion', 'A scarecrow', 'Batman', 'Mona Lisa', 'A plumber']
+const what = ['knitting a sweater', 'petting a cat', 'picking flowers', 'baking cookies', 'blowing birthday candles', 'hiking', 'drinking coffee', 'taking pictures', 'flying an invisible airplane', 'surfing']
+const where = ['at a bank.', 'at the hairdresser.', 'in a library.', 'at a baseball game.', 'in bed.', 'underwater.', 'on the moon.', 'in Texas.', 'at the top of a mountain.', 'under the bed.']
 
 function doodleBuzz() {
     let prompt = `${who[Math.floor(Math.random() * who.length)]} ${what[Math.floor(Math.random() * what.length)]} ${where[Math.floor(Math.random() * where.length)]}`
@@ -8,3 +8,5 @@ function doodleBuzz() {
 }
 
 console.log(doodleBuzz())
+
+console.log(who.length, what.length, where.length)
